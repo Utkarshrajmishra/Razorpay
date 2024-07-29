2faeb4890fa6896d13c66da5ca655a0ed8017613
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-07-29 17:37:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Popins:['Popins',"sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
